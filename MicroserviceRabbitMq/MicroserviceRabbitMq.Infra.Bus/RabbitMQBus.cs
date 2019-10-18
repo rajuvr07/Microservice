d318c1f3a0932251095ea25c2712eaa475f8cbf5
450251 <450251@PC414958>
2019-10-18 19:36:06 +0530

    Intermidate Checkin

---
+++ MicroserviceRabbitMq/MicroserviceRabbitMq.Infra.Bus/RabbitMQBus.cs
@@ -0,0 +1,50 @@
+﻿using MediatR;
+using MicroserviceRabbitMq.Domain.Core.Bus;
+using MicroserviceRabbitMq.Domain.Core.Commands;
+using MicroserviceRabbitMq.Domain.Core.Events;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroserviceRabbitMq.Infra.Bus
+{
+    public sealed class RabbitMQBus : IEventBus
+    {
+        private readonly IMediator _mediator;
+        private readonly Dictionary<string, List<Type>> _handlers;
+        private readonly List<Type> _eventTypes;
+
+        public RabbitMQBus(IMediator mediator)
+        {
+            _mediator = mediator;
+            _handlers = new Dictionary<string, List<Type>>();
+            _eventTypes = new List<Type>();
+
+        }
+        public Task SendCommand<T>(T command) where T : Command
+        {
+            return _mediator.Send(command);
+        }
+
+        public void Publish<T>(Task @event) where T : Event
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                var eventName = @event.GetType().Name;
+                channel.QueueDeclare(eventName, false, false, false, null);
+                
+            }
+        }
+               
+        public void Subscribe<T, TH>()
+            where T : Event
+            where TH : IEventHandler<T>
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
