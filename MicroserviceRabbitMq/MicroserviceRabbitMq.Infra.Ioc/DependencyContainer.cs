20643b6a2993d900665a35304449f4fa8c4f940f
450251 <450251@PC414958>
2019-10-21 15:53:49 +0530

    Initial Dependency project commit

---
+++ MicroserviceRabbitMq/MicroserviceRabbitMq.Infra.Ioc/DependencyContainer.cs
@@ -0,0 +1,18 @@
+﻿using MicroserviceRabbitMq.Domain.Core.Bus;
+using MicroserviceRabbitMq.Infra.Bus;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroserviceRabbitMq.Infra.Ioc
+{
+   public  class DependencyContainer
+    {
+
+        public static void RegisterServices(IServiceCollection services)
+        {
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
