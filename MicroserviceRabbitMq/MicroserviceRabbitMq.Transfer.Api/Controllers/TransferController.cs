7fecdee2158c29e4d609f6d040c2b5a640c86eea
450251 <450251@PC414958>
2019-11-07 16:55:15 +0530

    Added missed files

---
+++ MicroserviceRabbitMq/MicroserviceRabbitMq.Transfer.Api/Controllers/TransferController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MicroserviceRabbitMq.Transfer.Application.Interfaces;
+using MicroserviceRabbitMq.Transfer.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MicroserviceRabbitMq.Transfer.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransferController : ControllerBase
+    {
+        private readonly ITransferService _transferService;
+        public TransferController(ITransferService transferService)
+        {
+            _transferService = transferService;
+        }
+        
+        // GET api/transfer
+        [HttpGet]
+        public ActionResult<IEnumerable<TransferLog>> Get()
+        {
+            return Ok(_transferService.GetTransferLogs());
+        }
+
+        
+    }
+}
---
+++ MicroserviceRabbitMq/MicroserviceRabbitMq.Transfer.Data/Repositoy/TransferRepository.cs
@@ -0,0 +1,27 @@
+﻿using MicroserviceRabbitMq.Transfer.Data.Context;
+using MicroserviceRabbitMq.Transfer.Domain.Interfaces;
+using MicroserviceRabbitMq.Transfer.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroserviceRabbitMq.Transfer.Data.Repository
+{
+    public class TransferRepository : ITransferRepository
+    {
+        private TransferDbContext _ctx;
+        public TransferRepository(TransferDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+        public void Add(TransferLog transferLog)
+        {
+            _ctx.TransferLogs.Add(transferLog);
+            _ctx.SaveChanges();
+        }
+        public IEnumerable<TransferLog> GetTransferLogs()
+        {
+            return _ctx.TransferLogs;
+        }
+    }
+}
