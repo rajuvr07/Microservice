2e497ccb9ba275df91b8543ab6ffbfe9de614d32
450251 <450251@PC414958>
2019-12-12 18:17:36 +0530

    Micros service final commit

---
+++ MicroserviceRabbitMq/MicroserviceRabbitMq.MVC/Services/ITransferService.cs
@@ -0,0 +1,13 @@
+﻿using MicroserviceRabbitMq.MVC.Models.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MicroserviceRabbitMq.MVC.Services
+{
+    public interface ITransferService
+    {
+        Task Transfer(TransferDto transferDto);
+    }
+}
